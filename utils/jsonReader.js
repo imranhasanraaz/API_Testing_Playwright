a6109baaa4d492cf87ea2aa134294f2d93f855dc
Imran Hasan <imranhasanraaz@gmail.com>
2023-11-30 17:57:32 +0600

    Update jsonReader.js

--- utils/jsonReader.js
+++ utils/jsonReader.js
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 class JsonDataReader {
     constructor() {
         try {
-            const filePath = 'resources\\testdata.json';
+            const filePath = 'resources/testdata.json';
             const fileContent = readFileSync(filePath, 'utf-8');
             this.data = JSON.parse(fileContent);
         } catch (error) {
